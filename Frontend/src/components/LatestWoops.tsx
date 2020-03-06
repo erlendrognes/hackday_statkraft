ca0c0932382b7b2a9875f6561a7d02b2ec52ac96
Erlend Rognes <u40436@energycorp.com>
2020-03-06 16:10:56 +0100

    Whop style

--- Frontend/src/components/LatestWoops.tsx
+++ Frontend/src/components/LatestWoops.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { ListItem, ListItemText } from '@material-ui/core';
-import { IWhoop } from 'models/whoop';
-import moment from 'moment';
+import React from "react";
+import { Typography, ListItem, ListItemText } from "@material-ui/core";
+import { IWhoop } from "models/whoop";
+import moment from "moment";
 
-const LatestWoops: React.FC<{whoop: IWhoop}> = ({ whoop }) => {
-  const header = whoop.name + " - "  + moment(whoop.utcTick).format("DD.MM.YYYY - HH:mm");
-   return (
-    <ListItem>
-      <ListItemText
-        primary={header} secondary={whoop.body } />
-    </ListItem>
-  )
-}
+const LatestWoops: React.FC<{ whoop: IWhoop }> = ({ whoop }) => {
+    const header = whoop.name + " - " + moment(whoop.utcTick).format("DD.MM.YYYY - HH:mm");
+    return (
+        <ListItem>
+            <ListItemText>
+                <Typography variant="h6" align="center">
+                    {whoop.name}
+                </Typography>
+                <Typography variant="body1" align="left">
+                    {whoop.body}
+                </Typography>
+                <Typography variant="body2" align="right">
+                    <i>{moment(whoop.utcTick).format("DD.MM.YYYY - HH:mm")}</i>
+                </Typography>
+            </ListItemText>
+        </ListItem>
+    );
+};
 
 export default LatestWoops;
--- Frontend/src/screens/Dashboard.tsx
+++ Frontend/src/screens/Dashboard.tsx
@@ -1,68 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Grid, Paper, List } from '@material-ui/core'
-import LatestWoops from 'components/LatestWoops';
-import api from 'utils/api-client';
-import { IWhoop } from 'models/whoop';
+import React, { useState, useEffect } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { Grid, Paper, List } from "@material-ui/core";
+import LatestWoops from "components/LatestWoops";
+import api from "utils/api-client";
+import { IWhoop } from "models/whoop";
 import _ from "lodash";
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
+    createStyles({
+        root: {
+            flexGrow: 1,
+            padding: theme.spacing(3)
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: "center",
 
-      height: "100%",
-    },
-  }),
+            height: "100%"
+        }
+    })
 );
 
 const Dashboard: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [whoops, setWhoops] = useState<IWhoop[]>([]);
-  const [lastFiveWhoops, setLastFiveWhoops] = useState<IWhoop[]>([]);
+    const [whoops, setWhoops] = useState<IWhoop[]>([]);
+    const [lastFiveWhoops, setLastFiveWhoops] = useState<IWhoop[]>([]);
 
+    useEffect(() => {
+        api.Whoops.list().then(response => {
+            setWhoops(response);
+            let sorted: IWhoop[] = _.orderBy(response, (w: IWhoop) => w.utcTick, "desc");
+            setLastFiveWhoops(_.take(sorted, 5));
+        });
+    }, []);
 
-  useEffect(() => {
-    api.Whoops.list()
-      .then(response => {
-        console.log(response);
-        setWhoops(response);
-        let sorted = _.orderBy(response, w => w.utcTick, "desc")
-        setLastFiveWhoops(_.take(sorted, 5));
-      })
-  }, []);
-
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3} direction="row" alignItems="stretch">
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>
-            <List>
-              {_.map(lastFiveWhoops, whoop => (
-                <LatestWoops key={whoop.userId} whoop={whoop} />
-              ))}
-            </List>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>Data here...</Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>Data here...</Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>Data here...</Paper>
-        </Grid>
-      </Grid>
-
-    </div>
-  )
-}
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3} direction="row" alignItems="stretch">
+                <Grid item xs={12} md={6}>
+                    <Paper className={classes.paper}>
+                        <List>
+                            {_.map(lastFiveWhoops, (whoop: IWhoop) => (
+                                <LatestWoops key={whoop.userId} whoop={whoop} />
+                            ))}
+                        </List>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper className={classes.paper}>Data here...</Paper>
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    <Paper className={classes.paper}>Data here...</Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
 
 export default Dashboard;
